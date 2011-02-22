bf9636902987a326e67bda5bb9a15e78116f7b79
briza.tomas@gmail.com <briza.tomas@gmail.com@75287cee-a4e2-3cc7-29e4-2e0bee777cd7>
2011-02-22 21:01:08 +0000

    vlakna

--- uiSemestralniPrace/src/ui/aplikace/UiSemPrace.java
+++ uiSemestralniPrace/src/ui/aplikace/UiSemPrace.java
@@ -5,6 +5,9 @@
 
 package ui.aplikace;
 
+import ui.alg.Hra;
+import ui.alg.StavHry;
+
 /**
  *
  * @author notebook
@@ -16,6 +19,13 @@ public class UiSemPrace {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Hra hra1 = new Hra();
+        hra1.setDefault();
+        Hra hra2 = new Hra();
+        hra2.setDefault();
+        StavHry stavHry1 = new StavHry(hra1, null, 0);
+        StavHry stavHry2 = new StavHry(hra2, null, 0);
+        stavHry1.porovnejStavy(stavHry2);
     }
 
 }
