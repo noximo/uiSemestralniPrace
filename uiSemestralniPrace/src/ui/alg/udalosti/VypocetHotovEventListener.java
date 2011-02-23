4cd73ce1f66894e37960576e75b598c5f3690793
briza.tomas@gmail.com <briza.tomas@gmail.com@75287cee-a4e2-3cc7-29e4-2e0bee777cd7>
2011-02-23 21:09:19 +0000

    Finalni verze, vse snad konecne funguje

---
+++ uiSemestralniPrace/src/ui/alg/udalosti/VypocetHotovEventListener.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ui.alg.udalosti;
+
+import java.util.EventListener;
+
+/**
+ *
+ * @author notebook
+ */
+public interface VypocetHotovEventListener extends EventListener {
+    public void VypocetHotovEventOccurred(VypocetHotovEvent evt);
+}
